60d376484260984d748307137ef1bd1def0b8f4a
Kris Austin <kris.austin@draft2digital.com>
2024-12-03 11:45:15 -0600

    Day 3

--- python/day03/day03_part2.py
+++ python/day03/day03_part2.py
@@ -2,10 +2,10 @@ import re
 
 f = open("day03_input.txt").read()
 
-cleaned = re.sub(r"don't\(\).*?(do\(\)|$)","", f, flags=re.DOTALL)
+cleaned = re.sub(r"don't\(\).*?(do\(\)|$)", "", f, flags=re.DOTALL)
 
 total = 0
-for match in re.findall(r"mul\(?(\d+),?(\d+)\)", cleaned):
+for match in re.findall(r"mul\((\d+),(\d+)\)", cleaned):
     total += int(match[0]) * int(match[1])
 
 print(total)
