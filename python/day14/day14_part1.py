21c5328d7291f69433dfa0bc5eb9b4b1ff33812b
Kris Austin <kris.austin@draft2digital.com>
2024-12-15 08:54:20 -0600

    Day 14

---
+++ python/day14/day14_part1.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Robot:
+    x_position: int
+    y_position: int
+    x_velocity: int
+    y_velocity: int
+
+    def get_location(self, map_x_size, map_y_size, time):
+        x = (self.x_position + self.x_velocity * time) % map_x_size
+        y = (self.y_position + self.y_velocity * time) % map_y_size
+        return x, y
+
+robots = []
+
+lines = open("day14_input.txt").readlines()
+for line in lines:
+    position, velocity = line.split()
+    position = position.replace("p=", "").split(",")
+    velocity = velocity.replace("v=", "").split(",")
+    new_robot = Robot(int(position[0]), int(position[1]), int(velocity[0]), int(velocity[1]))
+    robots.append(new_robot)
+
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+
+max_x = 101
+max_y = 103
+
+for robot in robots:
+    x_loc, y_loc = robot.get_location(max_x, max_y, 100)
+    if 0 <= x_loc < max_x // 2 and 0 <= y_loc < max_y // 2:
+        q1 += 1
+    elif max_x // 2 < x_loc < max_x and 0 <= y_loc < max_y // 2:
+        q2 += 1
+    elif 0 <= x_loc < max_x // 2 and max_y // 2 < y_loc < max_y:
+        q3 += 1
+    elif max_x // 2 < x_loc < max_x and max_y // 2 < y_loc < max_y:
+        q4 += 1
+
+print(q1 * q2 * q3 * q4)
